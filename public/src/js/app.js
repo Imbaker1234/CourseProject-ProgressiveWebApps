9450284d0f4777942405580350adb7a590d1576d
Ian Baker <Ian.Baker@contractor.prometric.com>
2020-05-10 03:53:49 -0400

    Prior to Promise and Fetch Module.

---
+++ public/src/js/app.js
@@ -0,0 +1,15 @@
+let deferredPrompt;
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('../../sw.js')
+        .then(function () {
+            console.log('Service Worker Registered!')
+        });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+})
